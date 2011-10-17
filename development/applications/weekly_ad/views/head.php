868890d93768f01e3a38dfdf624ad00015a990b0
John Blythe <johnblythe@gmail.com>
2011-10-17 17:23:08 -0500

    moved around images, fixed css image calls, css and js calls in view(s)

--- development/applications/weekly_ad/views/head.php
+++ development/applications/weekly_ad/views/head.php
@@ -2,10 +2,10 @@
 	<meta charset="utf-8">
 	<title>Welcome to <? $name; ?>'s Weekly Ad</title>
 
-	<link rel='stylesheet' href="css/styles.css"></link>
+	<link rel='stylesheet' href="css/weekly_ad/styles.css"></link>
 	<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js'></script>
 	<script type="text/javascript" src="http://malsup.github.com/jquery.cycle.all.js"></script>
-	<script type='text/javascript' src='js/site.js'></script>
+	<script type='text/javascript' src='js/weekly_ad/site.js'></script>
 		
 <div id="fb-root"></div>
 <script src="http://connect.facebook.net/en_US/all.js"></script> 
