f2712c57dd133a0453e9a35a963c97cf62edb0be
Brian Kennedy <bjk964@gmail.com>
2011-10-17 19:05:36 -0500

    Added jQuery to tools subdomain

--- development/applications/specials/views/home.php
+++ development/applications/specials/views/home.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>Welcome to Specials</title>
 	<link rel="stylesheet" href="css/specials/styles.css" media="screen" charset="utf-8" />
-	<script src="http://tools.startreacing.us/jquery.latest.js" type="text/javascript"></script>
+	<script src="http://tools.startreaching.us/js/jquery.latest.js" type="text/javascript"></script>
 	<script src="js/specials/site.js" type="text/javascript"></script>
 </head>
 <body>
