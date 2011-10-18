03c6504542ecd901922c11ec4f0d338f6349d4a7
Brian Kennedy <bjk964@gmail.com>
2011-10-17 22:19:50 -0500

    styled app buttons and added to preprod

--- development/applications/specials/views/home.php
+++ development/applications/specials/views/home.php
@@ -5,8 +5,10 @@
 		<a id="bakery">Bakery</a>
 		<a id="produce">Produce</a>
 		<a id="meat">Meat</a>
+		<a id="seafood">Seafood</a>
+		<a id="floral">Floral</a>
 	</p>
-	
+	<div class="clear"></div><!--end clear-->
 	<div id="specials">
 		<div class="deli">
 			<h1>Deli Specials</h1>
@@ -19,6 +21,12 @@
 		</div>
 		<div class="meat">
 			<h1>Meat Specials</h1>
+		</div>
+		<div class="seafood">
+			<h1>Seafood</h1>
+		</div>
+		<div class="floral">
+			<h1>Floral</h1>
 		</div>			
 	</div><!--end specials-->
 </div><!--end content-->
